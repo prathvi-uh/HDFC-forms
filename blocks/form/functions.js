be6cfe1a6db6f06ebc7e515cbc811e3b7e4281ad
Prathvi <prathviu@adobe.com>
2026-04-16 19:47:29 +0530

    timerinvalid

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -267,5 +267,5 @@ function debugForm(globals) {
 
 // eslint-disable-next-line import/prefer-default-export
 export {
-  getFullName, days, submitFormArrayToString, maskMobileNumber, startOtpTimer, stopOtpTimer, handleResendOtp, handleOtpSuccess, debugForm,
+  getFullName, days, submitFormArrayToString, maskMobileNumber, startOtpTimer, stopOtpTimer, handleResendOtp, handleOtpSuccess, handleInvalidOtp, debugForm,
 };
