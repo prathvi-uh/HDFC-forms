40896785378e249a7f4ce8452813aa3f45c45b8f
Prathvi <prathviu@adobe.com>
2026-04-17 08:49:47 +0530

    chtimw

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -231,5 +231,5 @@ function debugForm(globals) {
 
 // eslint-disable-next-line import/prefer-default-export
 export {
-  getFullName, days, submitFormArrayToString, maskMobileNumber, startOtpTimer, stopOtpTimer, handleResendOtp, handleOtpSuccess, handleInvalidOtp,debugForm,
+  getFullName, days, submitFormArrayToString, maskMobileNumber, startOtpTimer, stopOtpTimer, handleResendOtp, handleOtpSuccess, debugForm,
 };
