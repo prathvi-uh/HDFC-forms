c53bedd35b2a13c6f02396e1f0548da5a87c0039
Prathvi <prathviu@adobe.com>
2026-04-27 21:59:40 +0530

    rtyh

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -366,9 +366,7 @@ function calculateEMI(globals) {
 
   const loanAmt = Math.round(getActualValueFromSlider(savedLoanRaw, loanTicks) / 1000) * 1000;
   const tenure = Math.round(getActualValueFromSlider(savedTenureRaw, tenureTicks));
-  globals.functions.setProperty(globals.form.offer.loantenure, {
-  value: tenure,
-  });
+  
   const annualRate = 10.09;
   const monthlyRate = annualRate / 12 / 100;
 
