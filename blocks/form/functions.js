16ab0be174e01d8904869dfc2f5c8c007bee89bf
Prathvi <prathviu@adobe.com>
2026-04-28 22:18:16 +0530

    ghdf

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -405,6 +405,21 @@ function calculateEMI(globals) {
  * @param {scope} globals
  * @returns {string}
  */
+function setReviewTenure(globals) {
+  var ticks = [12, 24, 36, 48, 60, 72, 84];
+
+  var raw = globals.form.offer.loantenure.valueOf();
+
+  if (raw === null || raw === undefined || raw === '') {
+    return '';
+  }
+
+  return ticks[Math.round(Number(raw))] + " months";
+}
+/**
+ * @param {scope} globals
+ * @returns {string}
+ */
 function setReviewLoanDetails(globals) {
   const ticks = [12, 24, 36, 48, 60, 72, 84];
 
